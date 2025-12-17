d76cee26a87552b49d2446020b1835532e679777
kaustubh.buch <kaustubh.buch@tatvasoft.com>
2025-12-17 18:05:04 +0530

    old file removed

--- resources/views/auth/includes/scripts.blade.php
+++
@@ -1,6 +0,0 @@
-<script src="{{ asset('js/jquery.min.js') }}"></script>
-<script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ asset('js/select2.min.js') }}"></script>
-<script src="{{ asset('js/general.js') }}"></script>
-<script src="{{asset('js/jquery.validate.min.js')}}"></script>
-<script src="{{asset('js/common.js')}}"></script>
